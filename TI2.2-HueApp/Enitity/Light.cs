192edd3c6b8dfe41bf342bdd60194124d5376eca
RD125632 <titat1993@gmail.com>
2015-12-01 13:40:55 +0100

    RGB Bind werkt, nu values checken

--- TI2.2-HueApp/Enitity/Light.cs
+++ TI2.2-HueApp/Enitity/Light.cs
@@ -1,32 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.ComponentModel;
 using System.Threading.Tasks;
+using TI2._2_HueApp.Connector;
 using TI2._2_HueApp.lib;
 using Windows.UI;
-using TI2._2_HueApp.Connector;
 
 namespace TI2._2_HueApp.Enitity
 {
-    public class Light
+    public class Light : INotifyPropertyChanged
     {
-	#region Properties region
+        #region Properties region
 
         private double _hue, _sat, _bri;
+        private bool _state;
+        private Color _color;
 
-	public Color RGBColor { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Color RGBColor
+        {
+            get { return _color; }
+            set
+            {
+                if (value != _color)
+                {
+                    _color = value;
+                    NotifyPropertyChanged(nameof(_color));
+                }
+            }
+
+        }
 
         public int ID { get; set; }
         public string Name { get; set; }
-        public bool State { get; set; }
+        public bool State
+        {
+            get { return _state; }
+            set
+            {
+                if (value != _state)
+                {
+                    _state = value;
+                    NotifyPropertyChanged(nameof(_state));
+                }
+            }
+
+        }
         public double Hue
         {
             get { return _hue; }
             set
             {
-                _hue = value;
-                CalculateColor();
+                if (value != this._hue)
+                {
+                    _hue = value;
+                    NotifyPropertyChanged(nameof(_hue));
+                    CalculateColor();
+                    NotifyPropertyChanged(nameof(RGBColor));
+                }
+
             }
         }
         public double Saturation
@@ -34,8 +65,13 @@ namespace TI2._2_HueApp.Enitity
             get { return _sat; }
             set
             {
-                _sat = value;
-                CalculateColor();
+                if (value != this._sat)
+                {
+                    _sat = value / 100.0;
+                    NotifyPropertyChanged(nameof(_sat));
+                    CalculateColor();
+                    NotifyPropertyChanged(nameof(RGBColor));
+                }
             }
         }
         public double Brightness
@@ -43,23 +79,36 @@ namespace TI2._2_HueApp.Enitity
             get { return _bri; }
             set
             {
-                _bri = value;
-                CalculateColor();
+                if (value != this._bri)
+                {
+                    _bri = value;
+                    NotifyPropertyChanged(nameof(_bri));
+                    CalculateColor();
+                    NotifyPropertyChanged(nameof(RGBColor));
+                }
             }
         }
 
-	#endregion
+        #endregion
 
-        
+
         public Light(int id, string name, bool state, int hue, int sat, int bri)
         {
             ID = id;
             Name = name;
             State = state;
             Hue = ((double)hue * 360.0f) / 65535.0f;
-            Saturation = (double)sat / 255.0f;
-            Brightness = (double)bri / 255.0f;
+            Saturation = (double)(sat / 255.0f);
+            Brightness = (double)(bri / 255.0f);
             CalculateColor();
+        }
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
 
         public void CalculateColor()
@@ -78,13 +127,13 @@ namespace TI2._2_HueApp.Enitity
         public string getHueToJson()
         {
             int value = (int)((Hue * 65535.0f) / 360);
-            return "{\"sat\"" + value.ToString() + "}";
+            return "{\"hue\"" + value.ToString() + "}";
         }
 
         public string getSatToJson()
         {
             int value = (int)(Saturation * 255.0f);
-            return "{\"hue\"" + value.ToString() + "}";
+            return "{\"sat\"" + value.ToString() + "}";
         }
 
         public string getBriToJson()
@@ -102,35 +151,36 @@ namespace TI2._2_HueApp.Enitity
         }
 
         #endregion
-        /*
+
         #region SendJsonToBridge region
 
+        /*
         public async Task SendStateToBridge(string relativeURIOfLight)
         {
-            await Global.Connector.HttpPut(relativeURIOfLight, getStateToJson());
+            await HueAPIConnector.HttpPut(relativeURIOfLight, getStateToJson());
         }
 
         public async Task SendHueToBridge(string relativeURIOfLight)
         {
-            await Global.Connector.HttpPut(relativeURIOfLight, getHueToJson());
+            await HueAPIConnector.HttpPut(relativeURIOfLight, getHueToJson());
         }
 
         public async Task SendSaturationToBridge(string relativeURIOfLight)
         {
-            await Global.Connector.HttpPut(relativeURIOfLight, getSatToJson());
+            await HueAPIConnector.HttpPut(relativeURIOfLight, getSatToJson());
         }
 
         public async Task SendBrightnessToBridge(string relativeURIOfLight)
         {
-            await Global.Connector.HttpPut(relativeURIOfLight, getBriToJson());
+            await HueAPIConnector.HttpPut(relativeURIOfLight, getBriToJson());
         }
 
         public async Task SendValuesToBridge(string relativeURIOfLight)
         {
-            await Global.Connector.HttpPut(relativeURIOfLight, getToJson());
+            await HueAPIConnector.HttpPut(relativeURIOfLight, getToJson());
         }
-        
+        */
         #endregion
-    */
+
     }
 }
