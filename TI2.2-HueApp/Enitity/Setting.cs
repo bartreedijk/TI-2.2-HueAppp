28f09bfe47a91669f2c3812a33fd4ac96cbf95ac
RD125632 <titat1993@gmail.com>
2015-12-04 11:45:42 +0100

    Added Setting object

---
+++ TI2.2-HueApp/Enitity/Setting.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TI2._2_HueApp.Enitity
+{
+    class Setting
+    {
+
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string IP { get; set; }
+        public int Port { get; set; }
+
+
+        public Setting(string name, string username, string ip, int port)
+        {
+            Name = name;
+            Username = username;
+            IP = ip;
+            Port = port;
+        }
+
+
+
+    }
+}
